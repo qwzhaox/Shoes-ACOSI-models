0830f5e9f17b50d9e87de4b9dd0bbc6f69d3e54d
Wenzhao Qiu <qwzhao@gl-login1.arc-ts.umich.edu>
2023-09-22 16:24:36 -0400

    add acosi

--- shoes-acosi/scripts/convert_json_txt.py
+++ shoes-acosi/scripts/convert_json_txt.py
@@ -1,6 +1,27 @@
+import argparse
 from json import load
 
-with open("shoes-acosi/review_splits.json") as f:
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_file", help="input json file", required=True)
+parser.add_argument(
+    "--output_file1",
+    help="output json file",
+    required=True,
+)
+parser.add_argument(
+    "--output_file2",
+    help="output json file",
+    required=True,
+)
+parser.add_argument(
+    "--output_file3",
+    help="output json file",
+    required=True,
+)
+
+args = parser.parse_args()
+
+with open(args.input_file) as f:
     review_splits = load(f)
 
 train = review_splits["train"]
@@ -27,11 +48,11 @@ train_str = "\n".join(train_str_list)
 test_str = "\n".join(test_str_list)
 val_str = "\n".join(val_str_list)
 
-with open("shoes-acosi/train.txt", "w") as f:
+with open(args.output_file1, "w") as f:
     f.write(train_str)
 
-with open("shoes-acosi/test.txt", "w") as f:
+with open(args.output_file2, "w") as f:
     f.write(test_str)
 
-with open("shoes-acosi/dev.txt", "w") as f:
+with open(args.output_file3, "w") as f:
     f.write(val_str)
