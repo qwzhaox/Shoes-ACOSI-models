2b0b1f352324126da417041b2ad8a870fc1eb548
Wenzhao Qiu <qwzhao@gl-login1.arc-ts.umich.edu>
2023-11-01 12:41:03 -0400

    acosi

---
+++ process-shoes-acosi/scripts/acosi_to_mvp.sh
@@ -0,0 +1,3 @@
+cp data/train.txt ../multi-view-prompting-acosi/data/acosi/shoes/train.txt
+cp data/test.txt ../multi-view-prompting-acosi/data/acosi/shoes/test.txt
+cp data/dev.txt ../multi-view-prompting-acosi/data/acosi/shoes/dev.txt
--- process-shoes-acosi/scripts/create_splits.sh
+++ process-shoes-acosi/scripts/create_splits.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 ../ECSR-shoes/scripts/create_splits.py -i ../ECSR-shoes/data/results.json --o data/splits.jsons
+python3 ../ECSR-shoes/scripts/create_splits.py -i ../ECSR-shoes/data/results.json --o data/splits.json
