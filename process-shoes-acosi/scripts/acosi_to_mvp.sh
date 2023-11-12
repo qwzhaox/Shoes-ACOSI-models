92628d45f7f0a81f7001d6749d1a660188e5b411
qwzhaox <qwzhao@umich.edu>
2023-11-12 16:54:59 -0500

    reorganize

--- process-shoes-acosi/scripts/acosi_to_mvp.sh
+++
@@ -1,3 +0,0 @@
-cp data/train.txt ../multi-view-prompting-acosi/data/acosi/shoes/train.txt
-cp data/test.txt ../multi-view-prompting-acosi/data/acosi/shoes/test.txt
-cp data/dev.txt ../multi-view-prompting-acosi/data/acosi/shoes/dev.txt
--- process-shoes-acosi/scripts/gen_mvp_category_dict.py
+++
@@ -1,5 +0,0 @@
-import json
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-org_file")
