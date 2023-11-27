897e1f062b68f86e76115d39da5fa75d4cf20d57
Wenzhao Qiu <qwzhao@gl-login1.arc-ts.umich.edu>
2023-11-27 12:53:38 -0500

    more runs

--- slurm/acos_extend/med.sh
+++ slurm/acos_extend/med.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --partition=spgpu
 #SBATCH --time=00-08:00:00
-#SBATCH --gpus=2
+#SBATCH --gpus=1
 #SBATCH --cpus-per-gpu=4
 #SBATCH --nodes=1 
-#SBATCH --mem-per-cpu=11500m
-#SBATCH --account=wangluxy0
+#SBATCH --mem-per-cpu=3900m
+#SBATCH --account=eecs595f23_class
 #SBATCH --mail-user=qwzhao@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --job-name=acos_extend_med
