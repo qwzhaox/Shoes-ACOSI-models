6f10742b2b04152c3aeaf0c4d2b66520259dd89e
Wenzhao Qiu <qwzhao@gl-login1.arc-ts.umich.edu>
2023-11-26 20:22:56 -0500

    slurm

---
+++ slurm/mvp/eval_unified_mvp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --partition=spgpu
+#SBATCH --time=00-01:00:00
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=1
+#SBATCH --nodes=1 
+#SBATCH --mem-per-cpu=11500m
+#SBATCH --account=wangluxy0
+#SBATCH --mail-user=qwzhao@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --job-name=eval_unified_mvp
+
+# conda
+module load python3.10-anaconda/2023.03
+pushd /home/qwzhao/src/absa/multi-view-prompting-acosi
+
+source /sw/pkgs/arc/python3.10-anaconda/2023.03/bin/activate
+conda activate mvp
+
+# run job
+bash scripts/eval_unified.sh
