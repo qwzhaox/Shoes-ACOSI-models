2474e6bda3ba070a5bd36ac859b54a2ae8963187
Wenzhao Qiu <qwzhao@gl-login1.arc-ts.umich.edu>
2023-11-28 21:06:27 -0500

    slurm

---
+++ slurm/acosi_extract/big.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --partition=spgpu
+#SBATCH --time=00-15:00:00
+#SBATCH --gpus=8
+#SBATCH --cpus-per-gpu=4
+#SBATCH --nodes=1 
+#SBATCH --mem-per-cpu=11500m
+#SBATCH --account=wangluxy1
+#SBATCH --mail-user=qwzhao@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --job-name=acosi_extract_big
+
+module load python cuda
+pushd /home/qwzhao/src/absa/EECS595Project
+
+source env/bin/activate
+
+bash scripts/bash/acosi_extract/big.sh
